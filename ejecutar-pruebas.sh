da8f98b81f143f6af19234af50fabd109ef9b7a1
Lubna Meziane Talha <lmeziane02@educarex.es>
2024-09-19 11:58:17 +0200

    C1

---
+++ ejecutar-pruebas.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Redirigir toda la salida a /dev/null
+exec > /dev/null 2>&1
+
+find . -mindepth 1 ! -name "$(basename "$0")" -delete
+
+# Ejecutar comandos de Git
+git init
+touch archivo1
+touch archivo2
+git add -A
+git commit -m "C1"
+git tag C1
+echo "Hola Mundo desde archivo1" >> archivo1
+git add -A
+git commit -m "C2"
+git tag C2
+echo "Hola Mundo desde archivo2" >> archivo2
+git add -A
+git commit -m "C3"
+git tag C3
+echo "Hola Mundo desde archivo3" >> archivo3
+git add -A

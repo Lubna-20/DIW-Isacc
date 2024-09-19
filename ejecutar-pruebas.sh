6e768c5b52bfc2c35a37c85481d86fe30b36b619
Lubna Meziane Talha <lmeziane02@educarex.es>
2024-09-19 12:06:01 +0200

    modificado

--- ejecutar-pruebas.sh
+++ ejecutar-pruebas.sh
@@ -12,13 +12,16 @@ touch archivo2
 git add -A
 git commit -m "C1"
 git tag C1
+
 echo "Hola Mundo desde archivo1" >> archivo1
 git add -A
 git commit -m "C2"
 git tag C2
+
 echo "Hola Mundo desde archivo2" >> archivo2
 git add -A
 git commit -m "C3"
 git tag C3
+
 echo "Hola Mundo desde archivo3" >> archivo3
 git add -A
